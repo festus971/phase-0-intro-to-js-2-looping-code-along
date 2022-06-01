aa052a296d609bc8aaa4567f7e2a1ea33cc2b88d
+ <+>
2022-06-01 19:07:32 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(newArray, event){
+    let thankYouCards=[]
+    for(let i=0; i < newArray.length; i++){
+        thankYouCards.push(`Thank you, ${newArray[i]}, for the wonderful ${event} gift!` )
+        
+    }
+    return thankYouCards
+}
+function countDown(Number){
+    while(Number > 0){
+        console.log(Number);
+        Number -=1;
+    }
+    console.log(Number);
+}
